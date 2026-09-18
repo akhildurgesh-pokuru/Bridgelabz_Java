98dc766561c21e2386c6d6bc009f188a5b310ce8
akhildurgesh-pokuru <akhildurgesh.pokuru@gmail.com>
2026-09-18 23:52:58 +0530

    commited some files

--- Bridgelabz_Java/src/javaProgrammingElement/CelsiustoFahrenheit.java
+++
@@ -1,7 +0,0 @@
-package Bridgelabz_Java.src.main.java.javaProgrammingElement;
-
-public class CelsiustoFahrenheit {
-    public static void main(String[] args){
-
-    }
-}
--- Bridgelabz_Java/src/javaProgrammingElement/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package javaProgrammingElement;
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.print("welcome to bridge labz");
-    }
-}
