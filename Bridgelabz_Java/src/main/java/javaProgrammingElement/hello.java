1c0c02ecb2316211373d463faec4bfda71e39bad
akhildurgesh-pokuru <akhildurgesh.pokuru@gmail.com>
2026-09-18 22:24:41 +0530

    created hello file

---
+++ Bridgelabz_Java/src/main/java/javaProgrammingElement/hello.java
@@ -0,0 +1,7 @@
+package Bridgelabz_Java.src.main.java.javaProgrammingElement;
+
+public class hello {
+    public static void main(String[] args){
+        System.out.print("welcome to bridge labz");
+    }
+}
